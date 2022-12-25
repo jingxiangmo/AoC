663fb3b62412cfe0be34c358d1b87eeed0539650
Jingxiang Mo <65676392+jingxiangmo@users.noreply.github.com>
2022-12-24 18:59:00 -0800

    sol day1 part2

--- day1/day1.cpp
+++ day1/day1.cpp
@@ -1,27 +1,29 @@
 #include <iostream>
 #include <fstream> 
+#include <vector>
 
 using namespace std;
 
 int main(){
 
+    vector<int> calories;
     string input; 
     ifstream MyReadFile("day1.txt");
-    int count, max = 0;
+
+    int count = 0;
 
     while (getline (MyReadFile, input)){ 
         if (input.empty())
         {
+        calories.push_back(count);
         count = 0;
         continue;
         }
-
         count = stoi(input) + count;
-        if( count > max)
-        {
-            max = count; 
-        }
     }
-    cout << max;
+
+    sort(calories.begin(), calories.end(), greater<int>());
+    cout << calories[0] + calories[1] + calories[2] ;
+
    return 0;
 }
